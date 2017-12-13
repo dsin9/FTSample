15548341e9c077d1b426494dd86d3f9a294160de
Singh, Deepak Kumar (IT Consultant) <DeSingh@consultantemail.com>
2017-12-13 04:32:42 +0000

    changes

--- src/components/release_orders/basket_list.js
+++ src/components/release_orders/basket_list.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import BasketListItem from "./basket_list_item";
+import { connect } from 'react-redux';
+import * as basketActions from '../../actions/basketActions';
 
-const BasketList = props => {
-  const basketItems = props.baskets.map(basket => {
+class BasketList extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentDidMount() {
+    this.props.fetchBaskets();
+  }
+
+  componentWillReceiveProps() {
+    window.previousLocation = this.props.location;
+  }
+
+  selectGrid(e) {
+    console.log(e.target)
+  }
+
+  render() {
+    const { label, goBack } = this.props;
+    console.log('goBack:'+goBack)
     return (
-      <BasketListItem
-        key={basket.id}
-        basket={basket}
-      />
+      <ul className="col-md-8 list-group">
+        {this.props.baskets.map((v, i) => {
+          return (
+            <BasketListItem
+              key={v.id}
+              basket={v}
+              onClick={this.selectGrid}
+            />
+          )
+        })
+        }
+      </ul>
     );
-  });
+  }
+}
 
-  return (
-    <ul className="col-md-8 list-group">
-      {basketItems}
-    </ul>
-  );
+const mapStateToProps = (state, ownProps) => {
+  return {
+    baskets: state.baskets
+  };
 };
 
-export default BasketList;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchBaskets: () => dispatch(basketActions.fetchBaskets())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BasketList);
--- src/components/release_orders/submissions_list_item.js
+++ src/components/release_orders/submissions_list_item.js
@@ -1,16 +1,52 @@
 import React from "react";
 
-const SubmissionsListItem = ({ basket, trackSubmissions }) => {
-  // const imageUrl = basket.snippet.thumbnails.default.url;
+const svgs = require.context('../../../node_modules/svg-country-flags/svg/', true, /\.svg$/)
+const keys = svgs.keys();
+
+const SubmissionsListItem = ({ submission }) => {
 
   return (
-    <li onClick={() => trackSubmissions(basket)} className="list-group-item">
-      <div className="basket-list media">
-        {/* <div className="media-left">
-          <img className="media-object" src={imageUrl} />
-        </div> */}
+    <li className="list-group-item  col-xs-12" style={{ width: '90%', marginLeft: '11px', marginBottom: '8px' }}>
+      <div className="basket-list media" style={{ marginLeft: '-22px', marginBottom: '10px' }}>
         <div className="media-body">
-          <div className="media-heading">{basket.intentSummary}</div>
+          <div className="row" style={{ width: '110%' }}>
+            <div className={ submission.side2 != undefined ? 'col-xs-7' : 'col-xs-8' }>
+              <span style={{ marginLeft: '10px', color: (submission.side == 'Buy') ? 'Blue' : 'Red', marginRight: '3px' }}>{submission.side}</span>
+              <img src={svgs("./" + submission.flag.toLowerCase() + ".svg")} style={{ width: '25px' }} />
+              <span style={{ marginLeft: '3px', marginRight: '3px' }}>{submission.symbol}</span>
+              <span style={{ marginRight: '3px' }}>{submission.size}</span>
+              <span style={{ marginRight: '3px' }}>{submission.measure}</span>
+              <span>|{submission.status}</span>
+            </div>
+            {submission.side2 != undefined &&
+              <div className="col-xs-5" style={{ display: submission.side2 != undefined ? 'block' : 'none' }}>
+                <span style={{ marginLeft: '-15px', marginRight: '3px' }}>vs.</span>
+                <span style={{ color: (submission.side2 == 'Buy') ? 'Blue' : 'Red', marginRight: '3px' }}>{submission.side2}</span>
+                <img src={svgs("./" + submission.flag2.toLowerCase() + ".svg")} style={{ width: '25px' }} />
+                <span style={{ marginLeft: '3px', marginRight: '3px' }}>{submission.symbol2}</span>
+                <span style={{ marginRight: '3px' }}>{submission.remainingSize2}</span>
+                <span style={{ marginRight: '3px' }}>{submission.measure2}</span>
+                <span>|{submission.status2}</span>
+              </div>
+            }
+          </div>
+          <div className="row" style={{ lineHeight: '40px', width: '150%' }}>
+          <div className="col-xs-8">
+            <span style={{ marginLeft: '15px' }}>% Executed:{submission.pctExecuted}</span>
+            <span>,&nbsp;Avg. Price:{submission.avgPrice}</span>
+            </div>
+            {
+              (submission.status !== 'Completed') &&
+              <button type="button" className="btn btn-primary" style={{ height: '25px', lineHeight: '10px', marginLeft: '300px', marginTop: '-90px', marginBottom: '-10px' }}>Cancel</button>
+            }
+          </div>
+          <div className="row" style={{ marginTop: (submission.status === 'Completed') ? '0px' : '-30px' }}>
+            <span style={{ marginLeft: '15px' }}>Age:{submission.age}</span>
+            <span>&nbsp;|&nbsp;Trader:{submission.traderInitial}</span>
+            &nbsp;<span style={{ marginLeft: '130px' }}>
+              <a href="tel:${submission.traderPhone}">{submission.traderPhone}</a>
+            </span>
+          </div>
         </div>
       </div>
     </li>
